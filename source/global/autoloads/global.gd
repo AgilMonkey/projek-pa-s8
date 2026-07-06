3a5df62a85ad348315b4659b52213607237350eb
agil <muhammadagilfadilah@gmail.com>
2026-07-06 17:56:54 +0800

    add global autoload with NetworkChannel enum

---
+++ source/global/autoloads/global.gd
@@ -0,0 +1,9 @@
+## This is a global autoload specific for this game. It'll handle common 
+## constants and function both for client and server
+extends Node
+
+
+enum NetworkChannel {
+	MAIN = 0,
+	CHAT = 1
+}
