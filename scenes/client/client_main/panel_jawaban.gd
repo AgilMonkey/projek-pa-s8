c9c95f9dab2409fe21016d65d92e749ab68ba6f4
agil <muhammadagilfadilah@gmail.com>
2026-07-13 23:33:36 +0800

    change some names

--- scenes/client/client_main/panel_jawaban.gd
+++ scenes/client/client_main/panel_jawaban.gd
@@ -13,9 +13,9 @@ func _can_drop_data(_position, _data_word):
 	return _data_word is GrabableWord
 
 
-func _drop_data(at_position: Vector2, data_word: Variant) -> void:
-	if not data_word is GrabableWord: return
-	data_word.reparent(panel_jawaban_h_flow_container)
+func _drop_data(_at_position: Vector2, _data_word: Variant) -> void:
+	if not _data_word is GrabableWord: return
+	_data_word.reparent(panel_jawaban_h_flow_container)
 
 
 func _panel_jawaban_h_flow_container_child_order_changed():
