e28783f16ff47fded151134e5423b8c7a68f19c5
agil <muhammadagilfadilah@gmail.com>
2026-07-18 03:05:06 +0800

    added anim to button

--- scenes/components/world_interact_button/world_interact_button.gd
+++ scenes/components/world_interact_button/world_interact_button.gd
@@ -5,6 +5,7 @@ extends Area2D
 
 signal button_pressed
 
+## A quick reminder that button texture best perform at 32x32
 @export var button_icon: Texture2D
 
 @onready var world_button: WorldButton = $WorldButton
@@ -19,12 +20,23 @@ func _ready() -> void:
 	body_exited.connect(_body_exited)
 
 
-func _this_player_client_entered(body: CharacterBody2D):
+func _this_player_client_entered(_body: CharacterBody2D):
+	world_button.modulate.a = 0.0
+	world_button.position.y = 20.0
+	
+	var _tween := create_tween()
+	_tween.tween_property(world_button, "modulate:a", 1.0, 0.1)
+	_tween.parallel().tween_property(world_button, "position:y", 0.0, 0.1)
+	
 	world_button.show()
 
 
-func _this_player_client_exited(body: CharacterBody2D):
-	world_button.hide()
+func _this_player_client_exited(_body: CharacterBody2D):
+	var _tween := create_tween()
+	_tween.tween_property(world_button, "modulate:a", 0.0, 0.1)
+	_tween.parallel().tween_property(world_button, "position:y", 20.0, 0.1)
+	
+	_tween.tween_callback(world_button.hide)
 
 
 func _body_entered(body: Node2D):
